e7a3fba57ec8bcfe3ecd6b496ddf51d764251776
jiananlan <98745436+jiananlan@users.noreply.github.com>
2025-07-19 14:50:46 +0800

    Update auto_update.py

--- auto_update.py
+++ auto_update.py
@@ -188,12 +188,10 @@ def process_new_file(file_path):
     html_template.replace("Replace_me_as_time",part1)
     html_template.replace("Replace_me_as_location", part2)
     html_template.replace("Replace_me_as_pg_name", part3)
-    with open(os.path.join("./docs/pages", f"page{file_random_name.replace('.jpg','')}.html"),'w',encoding='utf-8') as f:
+    with open(os.path.join("./docs/pages", f"Page{file_random_name.replace('.jpg','')}.html"),'w',encoding='utf-8') as f:
         f.write(html_template)
-        
-    
-    
-    
+    print(f"[Success] write page:{os.path.join("./docs/pages", f"Page{file_random_name.replace('.jpg','')}.html"}")
+
     # TODO:  后续逻辑可以继续写在这里
 
 
